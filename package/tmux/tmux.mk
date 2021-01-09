2d291d0414484c7f3456b91cc05e8140c6f500c4
lets code <luhux@outlook.com>
2021-01-09 13:51:31 +0800

    tmux不應該當作shell

--- package/tmux/tmux.mk
+++ package/tmux/tmux.mk
@@ -15,10 +15,10 @@ TMUX_IGNORE_CVES += CVE-2020-27347
 
 # Add /usr/bin/tmux to /etc/shells otherwise some login tools like dropbear
 # can reject the user connection. See man shells.
-define TMUX_ADD_TMUX_TO_SHELLS
-	grep -qsE '^/usr/bin/tmux$$' $(TARGET_DIR)/etc/shells \
-		|| echo "/usr/bin/tmux" >> $(TARGET_DIR)/etc/shells
-endef
-TMUX_TARGET_FINALIZE_HOOKS += TMUX_ADD_TMUX_TO_SHELLS
+#define TMUX_ADD_TMUX_TO_SHELLS
+#	grep -qsE '^/usr/bin/tmux$$' $(TARGET_DIR)/etc/shells \
+#		|| echo "/usr/bin/tmux" >> $(TARGET_DIR)/etc/shells
+#endef
+#TMUX_TARGET_FINALIZE_HOOKS += TMUX_ADD_TMUX_TO_SHELLS
 
 $(eval $(autotools-package))

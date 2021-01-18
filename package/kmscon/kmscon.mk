62371caf42edb59f7841c74c1db4cf9881386dcd
luhux <luhux@outlook.com>
2021-01-18 05:07:08 +0000

    package: add kmscon

---
+++ package/kmscon/kmscon.mk
@@ -0,0 +1,13 @@
+################################################################################
+#
+# kmscon
+#
+################################################################################
+
+KMSCON_VERSION = 01dd0a231e2125a40ceba5f59fd945ff29bf2cdc
+KMSCON_SITE = $(call github,Aetf,kmscon,$(KMSCON_VERSION))
+KMSCON_AUTORECONF = YES
+KMSCON_DEPENDENCIES += host-automake host-autoconf host-libtool libxkbcommon libtsm udev libdrm 
+
+
+$(eval $(autotools-package))

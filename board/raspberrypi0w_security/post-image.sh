c91d4499d72d38339c195e4c9c0ad6824f0cf348
luhux <luhux@outlook.com>
2021-01-21 09:39:34 +0000

    delete unused code, fix script path

--- board/raspberrypi0w_security/post-image.sh
+++ board/raspberrypi0w_security/post-image.sh
@@ -20,17 +20,6 @@ dtoverlay=miniuart-bt
 __EOF__
 		fi
 		;;
-		--aarch64)
-		# Run a 64bits kernel (armv8)
-		sed -e '/^kernel=/s,=.*,=Image,' -i "${BINARIES_DIR}/rpi-firmware/config.txt"
-		if ! grep -qE '^arm_64bit=1' "${BINARIES_DIR}/rpi-firmware/config.txt"; then
-			cat << __EOF__ >> "${BINARIES_DIR}/rpi-firmware/config.txt"
-
-# enable 64bits support
-arm_64bit=1
-__EOF__
-		fi
-		;;
 		--gpu_mem_256=*|--gpu_mem_512=*|--gpu_mem_1024=*)
 		# Set GPU memory
 		gpu_mem="${arg:2}"
